43f9132c7ed7abe311bfa340543292b7978c3ebb
ElenaShamiryan <elen91@list.ru>
2022-10-04 16:23:38 +0300

    Произведение элементов списка от -N до N

--- ex4.py
+++ ex4.py
@@ -8,6 +8,6 @@ print(list)
 a = int(input('Введите позицию первого эллемента: '))
 b = int(input('Введите позицию второго эллемента: '))
 length = int(N*2 + 1)
-if 0 < a <= length and 0 < b <= length:
+if 0 <= a <= length and 0 <= b <= length:
     mult = list[a]*list[b]
     print(f'Произведение элементов на данных позициях равно {mult}')
